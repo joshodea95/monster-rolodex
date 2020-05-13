512802838aeb210808014b3a917b3474377810d4
Josh O’Dea <joshodea@Joshs-MBP.gateway>
2020-05-13 16:04:55 +1000

    Updates

---
+++ precache-manifest.1451b0272c66c28de6742b612ee24d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3553c512aa9d609645e88490f288f8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0fc0135b025a6a3d1f1a",
+    "url": "/monster-rolodex/static/css/main.1695cadd.chunk.css"
+  },
+  {
+    "revision": "82815bfcb9bf73d8a971",
+    "url": "/monster-rolodex/static/js/2.be6af004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.be6af004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc0135b025a6a3d1f1a",
+    "url": "/monster-rolodex/static/js/main.f098f2ff.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
